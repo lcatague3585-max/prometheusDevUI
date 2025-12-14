ebb2b1175709159e4a85b74ca83e8fcad4ae35d8
Mar <lcatague3585@gmail.com>
2025-12-14 23:58:01 +0400

    Create models index to export User, Course, and AuditLog

---
+++ pke-backend/models/index.js
@@ -0,0 +1,9 @@
+/**
+ * Models Index
+ */
+
+const User = require('./User');
+const Course = require('./Course');
+const AuditLog = require('./AuditLog');
+
+module.exports = { User, Course, AuditLog };
