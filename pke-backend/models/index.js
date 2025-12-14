69dc2e29eb0e8e8a30d565a9600cb00d478cb992
Mar <lcatague3585@gmail.com>
2025-12-14 23:58:50 +0400

    Delete pke-backend/models directory

--- pke-backend/models/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Models Index
- */
-
-const User = require('./User');
-const Course = require('./Course');
-const AuditLog = require('./AuditLog');
-
-module.exports = { User, Course, AuditLog };
