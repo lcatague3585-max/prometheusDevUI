9f7fad44390fed3aa7f0f15d456b525b27343ccc
Mar <lcatague3585@gmail.com>
2025-12-16 01:05:46 +0400

    Create index.js

---
+++ src/context/index.js
@@ -0,0 +1,9 @@
+/**
+ * Context Index
+ * 
+ * Central export for all context providers and hooks
+ */
+
+export { AuthProvider, useAuth } from './AuthContext'
+export { CourseProvider, useCourse, EMPTY_COURSE } from './CourseContext'
+export { PKEProvider, usePKE, INVOCATION_NAMES } from './PKEContext'
