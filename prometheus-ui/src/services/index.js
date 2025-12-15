ae017b57671fb804caf1476a82edf0e9e6ec9d75
Mar <lcatague3585@gmail.com>
2025-12-16 03:23:29 +0400

    Create index.js

---
+++ prometheus-ui/src/services/index.js
@@ -0,0 +1,5 @@
+export { default as authService } from './authService'
+export { default as courseService } from './courseService'
+export { default as pkeService } from './pkeService'
+export { default as exportService } from './exportService'
+export * from './api'
